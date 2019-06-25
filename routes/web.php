6d079ee7f023a2e088688d4c0d7befa14376d90b
Hirwa Felix <hirwa.felix@gmail.com>
2019-06-26 00:59:37 +0200

    Install voyager ...

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::group(['prefix' => '/'], function () {
+    Voyager::routes();
 });
